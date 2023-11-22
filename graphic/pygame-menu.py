e104ed1f34f5af90611db67447a41672be4bb15c
insamartin <martin.duguey@insa-cvl.fr>
2023-11-22 16:33:12 +0100

    multiple bug fixes and new menus
    bug fix handle event
    menu game screen

--- graphic/pygame-menu.py
+++ graphic/pygame-menu.py
@@ -2,38 +2,61 @@ import pygame
 import pygame_menu
 
 pygame.init()
-surface = pygame.display.set_mode((1200, 800))
-
-#main.mainloop(surface)
+surface = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+myfont=pygame_menu.font.FONT_MUNRO
+myfontsize = 30
+window_size = surface.get_size()
 
 def launch_simulation():
     pass
 
-def start_new_game():
-    start = pygame_menu.Menu('New Game',800,600,theme=pygame_menu.themes.THEME_GREEN)
-    start.add.text_input('Population de départ :', default='100')
-    start.add.text_input('Nouriture de départ :', default='100')
-    start.add.text_input('movement de bob :', default='1')
-    start.add.button('back to main menue',main_menu)
-    start.add.button('Quit', pygame_menu.events.EXIT)
-    start.add.button('start',launch_simulation)
-    start.center_content()
-    start.mainloop(surface)
+mytheme = pygame_menu.themes.THEME_ORANGE.copy()
+mytheme.background_color=(0, 0, 0 ,0)
+mytheme.widget_alignment = pygame_menu.locals.ALIGN_RIGHT
+mytheme.widget_font =myfont
+mytheme.widget_font_size = myfontsize
+mytheme.widget_title_font = myfont
+pygame_menu.themes.THEME_BLUE.widget_font_size = myfontsize
+pygame_menu.themes.THEME_BLUE.widget_font = myfont
 
-def load_old_game():
-    load = pygame_menu.Menu('New Game',800,600,theme=pygame_menu.themes.THEME_SOLARIZED)
-    load.add.button('back to main menue',main_menu, align=pygame_menu.locals.ALIGN_RIGHT)
-    load.add.vertical_margin(50)
-    quitbutton = load.add.button('Quit', pygame_menu.events.EXIT)
-    startbutton = load.add.button('Start',launch_simulation)
-    load.mainloop(surface)
+game_screen = pygame_menu.Menu('Gaming',window_size[0],window_size[1],theme=mytheme)
+game_screen.add.button('Quit', pygame_menu.events.EXIT)
 
-def main_menu():
-    main = pygame_menu.Menu('EVOlution', 800, 600,theme=pygame_menu.themes.THEME_BLUE)
-    main.add.button('new game', start_new_game)
-    main.add.button('load game', load_old_game)
-    main.add.button('Option', start_new_game)
-    main.add.button('Quit', pygame_menu.events.EXIT)
-    main.mainloop(surface)
+start = pygame_menu.Menu('New Game',window_size[0],window_size[1],theme=pygame_menu.themes.THEME_GREEN)
+start.add.text_input('Population de départ :', default='100')
+start.add.text_input('Nouriture de départ :', default='100')
+start.add.text_input('movement de bob :', default='1')
+start.add.button('Quit', pygame_menu.events.EXIT)
+start.add.button('start', game_screen)
+start.center_content()
+
+load = pygame_menu.Menu('New Game',window_size[0],window_size[1],theme=pygame_menu.themes.THEME_SOLARIZED)
+load.add.vertical_margin(50)
+quitbutton = load.add.button('Quit', pygame_menu.events.EXIT)
+startbutton = load.add.button('Start',game_screen)
+
+main_menu = pygame_menu.Menu('EVOlution',window_size[0],window_size[1],theme=pygame_menu.themes.THEME_BLUE)
+
+main_menu.add.button('new game', start)
+main_menu.add.button('load game', load)
+main_menu.add.button('Option', start)
+main_menu.add.button('Quit', pygame_menu.events.EXIT)
+
+
+
     
-main_menu()
+if __name__ == '__main__':
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                break
+
+        # Draw the menu
+        surface.fill((75, 0, 50))
+
+        main_menu.update(events)
+        main_menu.draw(surface)
+
+        pygame.display.flip()
