94c57511c52560d0aecf0b48772005b4eb1a5a45
usamaerr <erraqiq.oussama13@gmail.com>
2023-11-28 21:15:59 +0100

    food

--- food.py
+++ food.py
@@ -2,4 +2,8 @@ class Food():
     def __init__(self,energy):
         self.energy=energy
      #ajouter une method comme is_dead()§§bob
+    def get_energy(self):return self.energy
+    def set_energy(self,eng):self.energy=eng 
+    def energy_food(self):
+        return  self.energy<=0
        
