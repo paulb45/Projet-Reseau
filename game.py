9ba7eae7119153db4a73a838c6d82678210789b8
usamaerr <erraqiq.oussama13@gmail.com>
2023-11-28 21:12:36 +0100

    game

--- game.py
+++ game.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD:Game.py
 import time
-import  Grid ,Food
-from bob import Bob
-from Food import Food
-import random
-=======
-import time, random
-
+import  grid ,food
 from bob import Bob
 from food import Food
-from config import *
->>>>>>> 3757a02d5aaae248e5c00c141c0ad002dfbf2438:game.py
+import random
 
 #var test bob 
 speed = 1
@@ -18,16 +10,14 @@ mass = 10
 E = 100
 speed_buff=1
 
-class Game():
-    def __init__(self,quantity_food,init_energy_food,nb_tick_day,P0,grid,nb_day): # METTRE DES VALEURS PAR DEFAUTS, nb_day ??? c'est quoi
+class GAME():
+    def __init__(self,quantity_food,init_energy_food,nb_tick_day,P0,grid,nb_day):
         self.init_quantity_food=quantity_food
         self.init_energy_food=init_energy_food
         self.init_nb_tick_day=nb_tick_day
-        self.P0=P0 # nombre des bobs a initialiser -> CHANGER LE NOM DE LA VARIABLE SI C'EST PAS CLAIR POUR VOUS !!!
+        self.P0=P0 #nombre des bobs a initialiser
         self.grid=grid
         self.nb_day=nb_day
-
-        # APPELER LES METHODES QUI SERONT LANCEES A LA CREATION DU JEU (init_bobs, ...)
         
     def get_quantity_food(self):
         return self.init_quantity_food
@@ -64,12 +54,11 @@ class Game():
         
         """
         for i in range(self.P0):
-            x, y = random.randint(0, N-1), random.randint(0, M-1) # La borne supérieur est comprise avec randint ?
+            x, y = random.randint(0, self.grid.N-1), random.randint(0, self.grid.M-1) 
             
             name= Bob( speed, mass, E, speed_buff)
             name.set_last_move((x,y))
             #f grid drtiha comme une liste ohna drtiha comme un dict
-            # !!! POURQUOI ON A UNE FONCTION CREATE BOB ET ON NE L'APPEL PAS !!!
             if (x, y) not in self.grid.grid:
                 self.grid.grid[(x, y)] = []
             self.grid.grid[(x,y)].append(name)
@@ -82,61 +71,44 @@ class Game():
             position (_type_): _description_
         """
         for i in range(self.get_quantity_food()):
-            x, y = random.randint(0, N-1), random.randint(0, M-1) # Meme remarque que à init_bobs
+            x, y = random.randint(0, self.grid.N-1), random.randint(0, self.grid.M-1)
             if (x, y) not in self.grid.grid:
                 self.grid.grid[(x, y)] = []
             self.grid.grid[x,y].append(Food(self.init_energy_food)) 
         
     def bob_play(self):
-<<<<<<< HEAD:Game.py
-        for bobs in self.grid.grid.items():
-            for bob in bobs: 
-                if isinstance(bob,Bob):
-                    position = self.grid.get_position(bob)
-                    ## tuer le bob si n as pas de l'energie sinon move
-                    if (bob.is_dead()):
-                        self.grid.grid[position].remove(bob)
-                        
-                    else:
-                        nb_bobs,nb_foods,bobs,foods=self.count(position[0],position[1])
-                        #s'il y a encore de la nourriture bob reste immobile
-                        if(nb_foods>0):
-                            bob.set_E(bob.get_E()-0.5)
-                        #sinon il se déplace    
-                        else:
-                            #bob choisi aléaroirement un mouvement parmis les mouvement dispo
-                            available_positions = self.grid.scan_around(position, bob.speed)
-                            mouvement=bob.move()
-                            while mouvement not in available_positions:
-                                mouvement=bob.move()
-                            self.grid.grid[position].remove(bob) #suppression de la dernière position
-                            self.grid.grid[mouvement].append(bob) #ajouter le bob pour la nouvelle position 
-                            #ici bob il a bien reussi son mouve
-                            #s'il y a plus qu'un bob dans la nouvelle case un seul qui va manger la nourriture
-                            if(nb_bobs==1 and nb_foods>0):
-                                eat=bob.eat()
-                    """" 
-                if available_positions:
-=======
-        # FONCTIONNE PAS
         # tuer le bob si n as pas de l'energie sinon move
         for bobs in self.grid.grid.items():
             for bob in bobs: 
-                if isinstance(bob,Bob): #Vérification si bob est une instance de la classe BOB
-                    # Pourquoi l'appeler bob avant de savoir si c'est un bob ?
+                if isinstance(bob,Bob): 
                     position = self.grid.get_position(bob)
-                    available_positions = self.grid.scan_around(position, bob.speed) #les places disponibles pour se déplacer 
-                if available_positions: # Bizarre ça
-                    # POURQUOI BOB PEUT FAIRE TOUTE LES ACTIONS ?! IL DOIT EN FAIRE QU'UNE SEULE
->>>>>>> 3757a02d5aaae248e5c00c141c0ad002dfbf2438:game.py
+                    
+                    nb_bobs,nb_foods,bobs,foods=self.count(position[0],position[1])
+                    #s'il y a encore de la nourriture bob reste immobile
+                    if(nb_foods>0):
+                        bob.set_E(bob.get_E()-0.5)
+                    #sinon il se déplace    
+                    else:
+                        #bob choisi aléaroirement un mouvement parmis les mouvement dispo
+                        available_positions = self.grid.scan_around(position, bob.speed)
+                        mouvement=bob.move()
+                        while mouvement not in available_positions:
+                            mouvement=bob.move()
+                        self.grid.grid[position].remove(bob) #suppression de la dernière position
+                        self.grid.grid[mouvement].append(bob) #ajouter le bob pour la nouvelle position 
+                        #ici bob il a bien reussi son mouve
+                        #s'il y a plus qu'un bob dans la nouvelle case un seul qui va manger la nourriture
+                        if(nb_bobs==1 and nb_foods>0):
+                            eat=bob.eat()
+                    """" 
+                if available_positions:
                     new_position = bob.move(self.grid.grid)
+                    bob.parthenogenesis()
                     self.grid.grid[position].remove(bob) #suppression de la dernière position
                     self.grid.grid[new_position].append(bob) #ajouter le bob pour la nouvelle position
                     bob.set_last_move(new_position) #MAJ du dernier mouvement du bob
-
-                    # bob.parthenogenesis()
-                    
-        
+        """
+                #if(count())
     def destroy_object(obj):
         """_Destroys the given object.__
 
@@ -146,20 +118,41 @@ class Game():
         del obj
         
     def day_play(self):
-        # A IMPLEMENTER
         """chaque jour d=100 ticks
            chaque jours f=200 points de nourriture
            Ef=100 energie de la nourriture
         """
-        tick=self.set_nb_tick_day(self.nb_tick_day) # pourquoi cette variable ? Pourquoi lui faire prendre exactement la même valeur qu'un truc qui existe déjà ?
-        fd_quantity=self.set_quantity_food(self.get_quantity_food) # Pareil qu'au dessus
+        tick=self.set_nb_tick_day(self.nb_tick_day)
+        fd_quantity=self.set_quantity_food(self.get_quantity_food)
         
         
         while tick>0:
             
             tick-=1
-            time.sleep(1) # A supprimer à la fin, sinon l'affichage graphique va pas aimer
-
+            time.sleep(1)
     def create_bob(self,Bob, x,y):
-        # FONCTIONNE PAS, KEYERROR quelque soit les valeurs x,y
         self.grid.grid[(x,y)].append(Bob)
+    
+    def count(self,x,y)->list:
+        """count et return les bobs et les foods d'une case données _
+
+        Args:
+            x (int):coord x
+            y (int):coord y
+
+        Returns:
+            list:nb_bobs,nb_foods,bobs,foods
+        """
+        nb_bobs,nb_foods=0,0
+        bobs=[]
+        foods=[]
+        if (x, y) not in self.grid.grid: return [0,0]
+        for elt in self.grid.grid[(x,y)]:
+            for e in elt:
+                if(isinstance(elt,Bob)):
+                    nb_bobs+=1
+                    bobs.append(elt)
+                elif(isinstance(elt,Food)): 
+                    nb_foods+=1
+                    foods.append(elt)
+        return [nb_bobs,nb_foods,bobs,foods]            
