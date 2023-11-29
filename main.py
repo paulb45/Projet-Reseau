6dc6a8877aeb74bd468ab7afa90ca76289a2fa71
Darouichay <145469762+Darouichay@users.noreply.github.com>
2023-11-29 15:08:54 +0100

    modif des testes de create bob

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ nb_day = 1
 
 game = Game(quantity_food,init_energy_food,nb_tick_day,P0,grid,nb_day)
 game.init_bobs()
-#game.create_bob(Bob(1,1,100,0), 1,1)
-affiche_map(game.grid.map)
+game.create_bob(Bob(1,1,100,0), 1,1)
+#affiche_map(game.grid.map)
 
 game.bob_play()
 
