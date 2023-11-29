6d643d1aab274e0f28d9aa1ce9bca697e832b571
paul <paul.baratin@etu.univ-tours.fr>
2023-11-29 15:40:14 +0100

    refonte main

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-import pygame
+from graphic.mainSurface import MainSurface
 
-from graphic.interface import Interface
-from graphic.cameraController import CameraController
-from graphic.eventController import EventController
-from config import *
-
-pygame.init()
-
-# Création de la fenêtre
-window = pygame.display.set_mode(window_size, pygame.RESIZABLE)
-window.fill("black")
-pygame.display.set_caption("Game Of Life")
-
-clock = pygame.time.Clock()
-
-screen = Interface(screen_size)
-
-camera = CameraController(screen)
-
-eventController = EventController(camera)
-
-screen.place_interface_in_middle(window)
-while True:
-    eventController.run_events()
-    
-    # RENDER YOUR GAME HERE
-    screen.render_game()
-    window.blit(camera.get_viewpoint(), (0,0))
-
-    pygame.display.flip()
-    clock.tick(max_framerate)
+MainSurface().run()
