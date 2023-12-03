a3d9a748a527af9f9ecc93936c8eeb08139a643e
usamaerr <erraqiq.oussama13@gmail.com>
2023-12-03 20:32:55 +0100

    CC

--- game.py
+++ game.py
@@ -94,7 +94,7 @@ class Game():
                     else:
                         mouvement=bob.move(bob.speed)
                         #si bob sort de la grill, il meurt
-                        if(mouvement[0]<0 or mouvement[0]>=self.grid.get_N() or mouvement[0]<0 or mouvement[1]>=self.grid.get_M()):
+                        if(mouvement[0]<0 or mouvement[0]>=self.grid.get_N() or mouvement[1]<0 or mouvement[1]>=self.grid.get_M()):
                             print("bob meur car il a sortie de la grille")
                             pass
                         self.grid.map[tuple(position)].remove(bob) #suppression de la dernière position
