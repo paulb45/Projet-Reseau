6d39818663a30adb782dfc0d296332b744443615
ThelioD <thelio.doucet@gmail.com>
2023-12-06 16:34:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from config import *
 from logic.game import Game
 window = MainSurface()
 
-# Initilisation de la logique
-quantity_food = 300
-init_energy_food = 10
-nb_tick_day = 5
-P0 = 5
-nb_day = 1
-game = Game(quantity_food,init_energy_food,nb_tick_day,P0,nb_day)
 
 # Initilisation de la logique
 quantity_food = 300
@@ -17,7 +10,6 @@ init_energy_food = 10
 nb_tick_day = 5
 P0 = 5
 nb_day = 1
-game = Game(quantity_food,init_energy_food,nb_tick_day,P0,nb_day)
 
 game = Game(Config.quantity_food,Config.energy_food,Config.nb_tick_day,Config.P0,Config.nb_day)
 # Gestion du menu ? -> où dans main surface
