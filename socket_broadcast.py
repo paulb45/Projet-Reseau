9f4a466d853d75fc339cf020b2a7467fdbb68d5e
paulb45 <p.baratinus@gmail.com>
2025-01-13 16:53:14 +0100

    interface entre le jeu et la partie réseau python

--- socket_broadcast.py
+++ socket_broadcast.py
@@ -1,4 +1,20 @@
 import socket
+from logic.bob import Bob
+
+def send_grid(grid):
+    """Extraire les déplacements des bobs pour les envoyer
+
+    Args:
+        grid (Grid): grille du jeu
+    """
+    for l in grid.map.keys():
+        for b in grid.map[l]:
+            if isinstance(b, Bob):
+                x1,y1 = [a+b for a,b in zip(l, b.last_move)]
+                x2,y2 = l
+                if (x1 != x2 or y1 != y2):
+                    s = f"DEPLACE|{x1:02},{y1:02}|{x2:02},{y2:02} \0"
+                    send_info_to_C(MSG=s.encode('ascii'))
 
 
 def send_info_to_C (portnum=55005, MSG=b'DEPLACE|x1,y1|x2,y2 \0') :
