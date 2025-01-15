9305ad3fb4e9bb6a5df56621bfb9597c367a50a6
thelio <thelio.doucet@gmail.com>
2025-01-15 11:09:56 +0100

    commentaires socket_broadcast

--- socket_broadcast.py
+++ socket_broadcast.py
@@ -21,16 +21,16 @@ def send_info_to_C (portnum=55005, MSG=b'DEPLACE|x1,y1|x2,y2 \0') :
     UDP_IP = "127.0.0.1"
     UDP_PORT = portnum
     MESSAGE = MSG
-    #print("UDP target IP: %s" % UDP_IP)
-    #print("UDP target port: %s" % UDP_PORT)
-    #print("message: %s" % MESSAGE)
+    print("UDP target IP: %s" % UDP_IP)
+    print("UDP target port: %s" % UDP_PORT)
+    print("message: %s" % MESSAGE)
  
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 if __name__ =='__main__':
     send_info_to_C(55005,b'DEPLACE|10,10|12,12')
