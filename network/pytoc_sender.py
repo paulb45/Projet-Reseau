e74de367bdfc69a5db1580f71ecb2ce46bbb13f6
insamartin <martin.duguey@insa-cvl.fr>
2025-01-17 08:18:38 +0100

    fonction pour mettre le texte au bon format

--- network/pytoc_sender.py
+++ network/pytoc_sender.py
@@ -1,4 +1,8 @@
 import socket
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from logic.bob import Bob
 
 def send_grid(grid):
@@ -16,21 +20,25 @@ def send_grid(grid):
                     s = f"DEPLACE|{x1:02},{y1:02}|{x2:02},{y2:02} \0"
                     send_info_to_C(MSG=s.encode('ascii'))
 
-def send_bob(pos, bob):
-    """Extraire le déplacement du bob pour l'envoyer
+def turnmsg_dpl_tobinary(oldposition, newposition):
+    oldx=str(format(oldposition[0],'b'))
+    oldy=str(format(oldposition[1],'b'))
+    newx=str(format(newposition[0],'b'))
+    newy=str(format(newposition[1],'b'))
+    msg = oldx+oldy+newx+newy
+    return msg
 
-    Args:
-        pos (tuple): tuple des coordonnées du bob
-        bob (Bob): bob à envoyer
-    """
-    x1, y1 = [a+b for a,b in zip(pos, bob.get_last_move())]
-    id = '0000000000'
-    # DPL	id	x1	y1	x2	y2
-    # s = f"DPL{id}{x1:5}{y1:5}{pos[0]:5}{pos[1]:5}"
-    s = f"DPL{x1:4}{y1:4}{pos[0]:4}{pos[1]:4}" # pour tester
+#def turnmsg_plc_tobinary():
 
-    send_info_to_C(MSG=s.encode('ascii'))
+#def turnmsg_eat_tobinary():
 
+#def turnmsg_atk_tobinary():
+
+#def turnmsg_dsp_tobinary():
+
+#def turnmsg_new_tobinary():    
+
+    
 
 def send_info_to_C (portnum=55005, MSG=b'DEPLACE|x1,y1|x2,y2 \0') :
     UDP_IP = "127.0.0.1"
@@ -44,4 +52,6 @@ def send_info_to_C (portnum=55005, MSG=b'DEPLACE|x1,y1|x2,y2 \0') :
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 if __name__ =='__main__':
-    send_info_to_C(55005,b'DEPLACE|10,10|12,12')
+    #send_info_to_C(55005,b'DEPLACE|10,10|12,12')
+    x=turnmsg_dpl_tobinary((12,13),(14,15))
+    print(x)
