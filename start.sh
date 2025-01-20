b14fa5c6f26d9352ae83622a7b191b384b12b4cb
NiCoChOcO <165646592+NiCoCh0c0@users.noreply.github.com>
2025-01-20 08:03:00 +0100

    réglage de conflit

--- start.sh
+++ start.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-<<<<<<< Updated upstream
-script_dir=$(dirname $0)
-cd network_c; make; cd ..
-
-# Start in a first console
-bash -c "
-./network_c/c2c 30001 40000;
-./network_c/c2py 20000 40001;
-python3 network/listener.py 20001;
-python3 network/pytoc_sender.py 30000;
-python3 main.py" &
-
-# Start in a second console
-bash -c "
-./network_c/c2c 3001 4000;
-./network_c/c2py 2000 4001;
-python3 network/listener.py 2001;
-python3 network/pytoc_sender.py 3000;
-python3 main.py" &
-
-=======
 # Définit le répertoire du script
 script_dir=$(dirname $0)
 
@@ -47,5 +26,4 @@ python3 $script_dir/main.py &
 wait" &
 
 # Attend la fin des deux processus
->>>>>>> Stashed changes
 wait
