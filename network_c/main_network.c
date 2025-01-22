4779b46512e135729a640a896d2d407ff2779f0e
MaxjunoDeLaJuno <maxjuno24@gmail.com>
2025-01-22 09:59:50 +0100

    ajout du socket set dans select de main_network

--- network_c/main_network.c
+++ network_c/main_network.c
@@ -63,29 +63,29 @@ int main(int argc, char *argv[]){
 
     printf("En écoute d'un message python en UDP sur le port %d...\n",  port_receive_py);
     
-    //Initialisation du socket_set
+
     FD_ZERO(&socket_set);
     FD_SET(socket_c2c,&socket_set); //socket_c2c = 3
     FD_SET(socket_c2py, &socket_set); //socket_c2py = 4
-
-
-
     //=================== Reception et envoi des messages ========================
     while (1) {
+            //Initialisation du socket_set
+        
 
-        socket_resolver = select(5,&socket_set,NULL,NULL,NULL); // 5 car 0,1 et 2 sont réservés
+
+        socket_resolver = select(5,&socket_set,&socket_set,NULL,NULL); // 5 car 0,1 et 2 sont réservés
         if(socket_resolver > 0){
 
             //================ c2c ==============================================
             if(FD_ISSET(socket_c2c,&socket_set)){
-                listen_socket(socket_c2c, message, MAX_BUF_SIZE, &from_addr_c2c, from_len_c2c, 0);
-                send_message(socket_c2c, message, &broadcast_addr, 0);
+                listen_socket(socket_c2c, message, MAX_BUF_SIZE, &from_addr_c2c, from_len_c2c, 1);
+                send_message(socket_c2c, message, &broadcast_addr, 1);
             }
 
             //================ c2py ==============================================
             if(FD_ISSET(socket_c2py,&socket_set)){
-                listen_socket(socket_c2py, message, MAX_BUF_SIZE, &from_addr_c2py, from_len_c2py, 0);
-                send_message(socket_c2py, message, &py_addr, 0); 
+                listen_socket(socket_c2py, message, MAX_BUF_SIZE, &from_addr_c2py, from_len_c2py, 1);
+                send_message(socket_c2py, message, &py_addr, 1); 
             }
         }   
     }
